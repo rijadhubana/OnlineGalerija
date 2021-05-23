bdcd0bec272abb435a0110f20657fabdb26d8de0
Rijad Hubana <rijad.hubana@edu.fit.ba>
2021-05-23 21:49:45 +0200

    dependency injectioni na controllerima

--- OnlineGalerija/Controllers/MongoController.cs
+++ OnlineGalerija/Controllers/MongoController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using OnlineGalerija.Helper;
+using OnlineGalerija.Models;
 
 namespace OnlineGalerija.Controllers
 {
     [Authorization(true, false)]
     public class MongoController : Controller
     {
+        private mongoDbContext _db;
+        public MongoController()
+        {
+            _db = new mongoDbContext();
+        }
         public IActionResult Index()
         {
+            //primjer pristupanja login sesiji
+            //var logiraniKorisnik = HttpContext.GetLogiraniKorisnik();
+            //string userNameSurname = logiraniKorisnik.mongoUser.mongoUser.namesurname;
             return View();
         }
     }
--- OnlineGalerija/Controllers/PostgreController.cs
+++ OnlineGalerija/Controllers/PostgreController.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OnlineGalerija.Helper;
+using OnlineGalerija.PostgresModels;
 
 namespace OnlineGalerija.Controllers
 {
     [Authorization(false, true)]
     public class PostgreController : Controller
     {
+        private postgresDbContext _db;
+        public PostgreController(postgresDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            //primjer pristupanja login sesiji
+            //var logiraniKorisnik = HttpContext.GetLogiraniKorisnik();
+            //string userNameSurname = logiraniKorisnik.postgreUser.postgreUser.NameSurname;
             return View();
         }
     }
